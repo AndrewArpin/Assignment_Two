84fb63b91ea77652497e1a3cc402cc66a442537f
Andrew Arpin <200398857@student.georgianc.on.ca>
2019-12-04 14:39:38 -0500

    Created Tester

---
+++ Assignment2Tester/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment2Tester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
